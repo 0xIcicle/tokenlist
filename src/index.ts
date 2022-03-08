565cdb0716d5f1e81a09b04fbcc4bf35cb69e596
icicle <0xicicle@protonmail.com>
2022-03-07 17:46:08 -0800

    update logos

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import fs from 'fs/promises';
+import {createCanvas, loadImage} from 'canvas';
 
 interface TokenDef {
     address: string;
@@ -6,7 +7,7 @@ interface TokenDef {
     name: string;
     symbol: string;
     decimals: number;
-    logoUri: string;
+    logoURI: string;
 }
 
 interface TokenList {
@@ -21,26 +22,38 @@ interface TokenList {
     tokens: TokenDef[]
 }
 
+const logoSize = 512;
+
+async function downloadLogo(url: string, outFile: string) {
+    const canvas = createCanvas(logoSize, logoSize);
+    const ctx = canvas.getContext('2d');
+    ctx.quality = 'best';
+    console.log(`Attempting to download ${url}`);
+    const image = await loadImage(url);
+    // If they're really small, treat them as pixel art and scale with nearest neighbour
+    if (image.width < logoSize / 4 || image.height < logoSize / 4) {
+        ctx.patternQuality = 'nearest';
+    }
+    ctx.drawImage(image, 0, 0, logoSize, logoSize);
+    const output = canvas.toBuffer('image/png');
+    await fs.writeFile(outFile, output);
+}
+
 async function main(): Promise<void> {
-    const tokens = JSON.parse((await fs.readFile('output.json')).toString()).data;
+    const tokens: TokenList = JSON.parse((await fs.readFile('icicle.tokenlist.json')).toString());
 
-    const tokenList: TokenList = {
-        name: '@icicle/tokenlist',
-        timestamp: new Date().toISOString(),
-        version: {
-            major: 1,
-            minor: 0,
-            patch: 0,
-        },
-        keywords: [
-            'icicle',
-            'finance',
-            'amm',
-        ],
-        tokens,
-    };
+    for (const token of tokens.tokens) {
+        if (token.logoURI) {
+            try {
+            await downloadLogo(token.logoURI, `./assets/${token.address}.png`);
+            } catch (e) {
+                console.log(e);
 
-    await fs.writeFile('icicle.tokenlist.json', JSON.stringify(tokenList, undefined, 2));
+            }
+
+        }
+
+    }
 }
 
 void main();
